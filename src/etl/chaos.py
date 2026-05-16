e6a93475bcac339159e4f2736d0cbe84284fdc15
Wilcson <wilcsonaraujo@outlook.com>
2026-05-16 16:01:27 -0300

    Added Chaos injection

---
+++ src/etl/chaos.py
@@ -0,0 +1,37 @@
+import logging
+import random
+
+import numpy as np
+import pandas as pd
+
+
+def simulate_traffic_spike(df):
+    df_aux = df.sample(frac=1.0, replace=True, random_state=1)
+    df = pd.concat([df, df_aux], ignore_index=True)
+    return df
+
+
+def simulate_system_outage(df):
+    df.loc[df.sample(frac=0.4).index, "response"] = 500
+    return df
+
+
+def simulate_empty_responses(df):
+    df.loc[df.sample(frac=0.7).index, "bytes"] = 0
+    return df
+
+
+def inject_chaos(df, probability=1.0):
+    logging.info("Deciding whether or not to apply chaos.")
+    chaos_functions = {
+        "traffic_spike": simulate_traffic_spike,
+        "system_outage": simulate_system_outage,
+        "empty_responses": simulate_empty_responses,
+    }
+    if random.random() < probability:
+        chaos_type = random.choice(list(chaos_functions.keys()))
+        logging.warning(f"!!! INJECTED CHAOS !!!: {chaos_type}")
+        return chaos_functions[chaos_type](df)
+
+    logging.info("Chaos don't applied.")
+    return df
