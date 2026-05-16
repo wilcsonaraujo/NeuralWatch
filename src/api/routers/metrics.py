7171d8648890805a7e525aa75e6533f45d9d8235
Wilcson <wilcsonaraujo@outlook.com>
2026-05-15 22:49:48 -0300

    Code improvements

--- src/api/routers/metrics.py
+++ src/api/routers/metrics.py
@@ -4,11 +4,13 @@ from src.api.schemas import HealthMetricsOutput
 
 router = APIRouter()
 
+
 @router.get("/health", response_model=HealthMetricsOutput)
 async def health_metrics_get_response():
-    return {"status": "ativo",
-            "service": "local host",
-            "version": "v1",
-            "environment": "dev",
-            "timestamp": datetime.now(datetime.timezone.utc)
-            }
+    return {
+        "status": "ativo",
+        "service": "local host",
+        "version": "v1",
+        "environment": "dev",
+        "timestamp": datetime.now(datetime.timezone.utc),
+    }
--- src/etl/extract.py
+++ src/etl/extract.py
@@ -1,14 +1,17 @@
+import os
 import pandas as pd
 import logging
-import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 file_log_01 = os.environ.get("FILE_LOG_01")
 
+
 def read_csv_file():
     try:
-        df = pd.read_csv(file_log_01, sep = '\t', encoding='latin1')
-        logging.info('File was read successfully.')
+        df = pd.read_csv(file_log_01, sep="\t", encoding="latin1")
+        logging.info("File was read successfully.")
         return df
     except FileNotFoundError:
         logging.error("File not found!")
-    
