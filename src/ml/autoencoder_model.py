a9ab553e216143114ee4beac48d355f71b27d897
Wilcson <wilcsonaraujo@outlook.com>
2026-05-18 19:24:51 -0300

    Developed autoencoder

--- src/ml/autoencoder_model.py
+++ src/ml/autoencoder_model.py
@@ -1,4 +1,10 @@
 import pandas as pd
 import tensorflow as tf
-import keras
+from keras import layers, models
 
+def build_autoencoder(input_dim, encoding_dim):
+    input_layer = layers.Input(shape=(input_dim,))
+    encoded = layers.Dense(encoding_dim, activation='relu')(input_layer)
+    decoded = layers.Dense(input_dim, activation='linear')(encoded)
+    autoencoder = models.Model(input_layer, decoded)
+    return autoencoder
